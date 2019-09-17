73416d4aa52c93913a4488ca1b8cba7fc383a563
Artem Pletnev <hoffange@gmail.com>
2019-09-17 22:31:39 +0400

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Основной класс для запуска приложения
@@ -6,9 +8,33 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Generator generator = new Generator();
-        int[] array = generator.generateArray(15,0,15);
+        int[] array = generator.generateArray(20,0,50);
         Arrays.stream(array).forEach(System.out::println);
 
+        List<Integer> dividedByThree = Arrays.stream(array).
+                boxed().
+                filter(x -> x % 3 == 0).
+                sorted().
+                peek(x -> System.out.format("divided by 3: %d%n", x)).
+                collect(Collectors.toList());
+
+        List<Integer> dividedBySeven = Arrays.stream(array).
+                boxed().
+                filter(x -> x % 7 == 0).
+                sorted().
+                peek(x -> System.out.format("divided by 7: %d%n", x)).
+                collect(Collectors.toList());
+
+        List<Integer> dividedByTwentyOne = Arrays.stream(array).
+                boxed().
+                filter(x -> x % 21 == 0).
+                sorted().
+                peek(x -> System.out.format("divided by 21: %d%n", x)).
+                collect(Collectors.toList());
+    }
+
+
+}
 
 
 //        Stream.of(120, 410, 85, 32, 314, 12, 5, 25, 77, 33).
@@ -16,6 +42,4 @@ public class Main {
 //                peek(x -> System.out.format("after filter: %d%n", x)).
 //                sorted().
 //                forEach(System.out::println);
-    }
-}
 
